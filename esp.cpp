20bc187e6d13709939524333b765d9737fd34bb3
N-tronics <62951161+N-tronics@users.noreply.github.com>
2021-02-10 13:52:49 +0530

    Create esp.cpp

---
+++ esp.cpp
@@ -0,0 +1,35 @@
+/*
+Adds analogWrite() of Atmel AVR boards to ESP boards plus some more functions
+Created by Nischay Bharadwaj
+Private library
+*/
+
+#include "esp.h"
+#include "Arduino.h"
+
+int res;
+
+void espDACSetup(int gpio, int chan, int resol, int freq) {
+	res = pow(2, resol);
+	ledcSetup(chan, freq, resol);
+	ledcAttachPin(gpio, chan);
+}
+
+void espAnalogWrite(int chan, int duty) {
+	ledcWrite(chan, duty);
+}
+
+void espAnalogWrite_DimLED(int chan, int del) {
+	for (int i = 0; i < res; ++i) {
+		espAnalogWrite(chan, i);
+		Serial.println(i);
+		delay(del);
+	}
+	delay(100);
+	for (int i = res - 1; i > 0; --i) {
+		espAnalogWrite(chan, i);
+		Serial.println(i);
+		delay(del);
+	}
+	delay(100);		
+}
