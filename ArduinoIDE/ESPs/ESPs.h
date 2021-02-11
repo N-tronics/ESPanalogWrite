00495f4b35b631ad5c4ce045fe2d439fa94ac354
N-tronics <62951161+N-tronics@users.noreply.github.com>
2021-02-11 08:23:29 +0530

    Update ESPs.h

--- ArduinoIDE/ESPs/ESPs.h
+++ ArduinoIDE/ESPs/ESPs.h
@@ -4,12 +4,19 @@ Created by Nischay Bharadwaj
 Private library
 */
 
-#ifndef espFuncs_h
-#define espFuncs_h
+#ifndef ESPs_h
+#define ESPs_h
 #include "Arduino.h"
 
-void espDACSetup(int gpio, int chan, int resol = 8, int freq = 1000);
-void espAnalogWrite(int chan, int duty);
-void espAnalogWrite_DimLED(int chan, int del = 10);
-
-#endif
+class ESPs {
+	private:
+		int res;
+		
+	public:
+		ESPs();
+		void espSetup(int gpio, int chan, int resol = 8, int freq = 1000);
+		void espAnalogWrite(int chan, int duty);
+		void espAnalogWrite_DimLED(int chan, int del = 10);
+		
+	#endif
+};
