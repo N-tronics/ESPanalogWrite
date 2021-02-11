f5a5e310e079d5aef895e14edf4a39f97e514a16
N-tronics <62951161+N-tronics@users.noreply.github.com>
2021-02-11 08:25:41 +0530

    Update ESPs.cpp

--- PlatformIO/ESPs/src/ESPs.cpp
+++ PlatformIO/ESPs/src/ESPs.cpp
@@ -7,30 +7,27 @@ Private library
 #include "ESPs.h"
 #include "Arduino.h"
 
-ESPs::ESPs (){}
-
 int res;
 
-void ESPs::espSetup(int gpio, int chan, int resol, int freq) {
+void espSetup(int gpio, int chan, int resol, int freq) {
 	res = pow(2, resol);
 	ledcSetup(chan, freq, resol);
 	ledcAttachPin(gpio, chan);
 }
 
-void ESPs::espAnalogWrite(int chan, int duty) {
+void espAnalogWrite(int chan, int duty) {
 	ledcWrite(chan, duty);
 }
 
-void ESPs::espAnalogWrite_DimLED(int chan, int del) {
-	for (int i = 0; i < res; ++i) {
+void espAnalogWrite_DimLED(int chan, int del) {
+	int i;
+	for (i = 0; i < res; ++i) {
 		espAnalogWrite(chan, i);
-		Serial.println(i);
 		delay(del);
 	}
 	delay(100);
-	for (int i = res - 1; i > 0; --i) {
+	for (i = res - 1; i > 0; --i) {
 		espAnalogWrite(chan, i);
-		Serial.println(i);
 		delay(del);
 	}
 	delay(100);
